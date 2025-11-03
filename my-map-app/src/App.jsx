ee6c78bf26d9ee1dd88f056e44716e9e3c6bf41f
Santiagogabh <santiagogabhernandez@gmail.com>
2025-11-03 09:18:21 -0500

    CAMBIOS EN EL HTML Y post.css.config

--- my-map-app/src/App.jsx
+++ my-map-app/src/App.jsx
@@ -1,16 +1,9 @@
-
-//import React from "react";
-//import OrganizationMap from "./Components/map/OrganizationMap";
-
-
-//export default function App() {
-//  return <OrganizationMap />;
-//}
+import 'leaflet/dist/leaflet.css';
 import React from "react";
 import OrganizationMap from "./components/map/OrganizationMap";
 
 function App() {
-  console.log("✅ App cargado con mapa"); // Verifica en consola si aparece este mensaje
+  console.log("✅ App cargado con mapa");
   return (
     <div className="min-h-screen bg-gray-50 p-10">
       <h1 className="text-3xl font-bold text-gray-800 mb-6">
